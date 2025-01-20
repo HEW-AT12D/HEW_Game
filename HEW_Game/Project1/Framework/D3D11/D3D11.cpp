86d14cd30d14468d1e1016c5a645da6d2a16ab47
kairi0825 <0wz3791578c4-8b@ezweb.ne.jp>
2025-01-20 16:39:48 +0900

    Revert "jfi"

--- HEW_Game/Project1/Framework/D3D11/D3D11.cpp
+++ HEW_Game/Project1/Framework/D3D11/D3D11.cpp
@@ -311,8 +311,7 @@ HRESULT D3D11::CompileShader(const char* szFileName, LPCSTR szEntryPoint, LPCSTR
 	return S_OK;
 }
 
-//ok4okrk4owe
-int i;
+
 //////////////////////////////////
 //			シェーダー系			//
 //////////////////////////////////
